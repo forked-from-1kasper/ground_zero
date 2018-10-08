cc5b4799e24041154f709a80947858830618d79a
Siegmentation <siegmentationfault@yandex.ru>
2018-10-08 21:58:32 +0700

    pushout

---
+++ pushout.lean
@@ -0,0 +1,23 @@
+namespace ground_zero
+universe u
+
+section
+  parameters {α β σ : Type u} (f : σ → α) (g : σ → β)
+  inductive pushout_rel : (α ⊕ β) → (α ⊕ β) → Prop
+  | mk {} : Π (x : σ), pushout_rel (sum.inl (f x)) (sum.inr (g x))
+  def pushout := quot pushout_rel
+end
+
+namespace pushout
+  variables {α β σ : Type u} {f : σ → α} {g : σ → β}
+  def inl (x : α) : pushout f g :=
+  quot.mk (pushout_rel f g) (sum.inl x)
+
+  def inr (x : β) : pushout f g :=
+  quot.mk (pushout_rel f g) (sum.inr x)
+
+  def glue (x : σ) : inl (f x) = inr (g x) :=
+  quot.sound (pushout_rel.mk x)
+end pushout
+
+end ground_zero
