47d0116e6f489b307a072e19f64114f516da377b
Siegmentation <siegmentationfault@yandex.ru>
2018-12-25 18:59:42 +0700

    renaming to get consistency with Infinity
    https://github.com/groupoid/cubical/blob/master/src/proto_path.ctt

--- ground_zero/cubical/path.lean
+++ ground_zero/cubical/path.lean
@@ -5,50 +5,57 @@ open ground_zero.HITs.interval (i₀ i₁ seg)
 namespace ground_zero.cubical.path
 universes u v
 
-@[refl] def refl {α : Type u} (a : α) : a ⇝ a := <i> a
-@[refl] def rfl {α : Type u} {a : α} : a ⇝ a := <i> a
+@[refl] def refl {α : Sort u} (a : α) : a ⇝ a := <i> a
+@[refl] def rfl {α : Sort u} {a : α} : a ⇝ a := <i> a
 
-@[symm] def symm {α : Type u} {a b : α} (p : a ⇝ b) : b ⇝ a :=
+@[symm] def symm {α : Sort u} {a b : α} (p : a ⇝ b) : b ⇝ a :=
 <i> p # −i
 postfix `⁻¹` := symm
 
-def funext {α : Type u} {β : α → Type v} {f g : Π (x : α), β x}
+abbreviation inv {α : Sort u} {a b : α} (p : a ⇝ b) := p⁻¹
+
+def funext {α : Sort u} {β : α → Sort v} {f g : Π (x : α), β x}
   (p : Π (x : α), f x ⇝ g x) : f ⇝ g :=
 <i> λ x, (p x) # i
 
-def cong {α : Type u} {β : Type v} {a b : α}
+def cong {α : Sort u} {β : Sort v} {a b : α}
   (f : α → β) (p : a ⇝ b) : f a ⇝ f b :=
 <i> f (p # i)
 
-def subst {α : Type u} {π : α → Type v} {a b : α}
+def ap {α : Sort u} {β : α → Sort v} (a x : α)
+  (f : α → β a) (b : β a) (p : a ⇝ x) : f a ⇝ f x :=
+<i> f (p # i)
+
+def subst {α : Sort u} {π : α → Sort v} {a b : α}
   (p : a ⇝ b) (x : π a) : π b :=
 coe (λ i, π (p # i)) x i₁
 
-abbreviation transport {α : Type u} (π : α → Type v) {a b : α}
+abbreviation transport {α : Sort u} (π : α → Sort v) {a b : α}
   (p : a ⇝ b) : π a → π b := subst p
 
-def transportconst {α β : Type u} : (α ⇝ β) → (α → β) :=
+def trans {α β : Sort u} : (α ⇝ β) → (α → β) :=
 transport id
+abbreviation coerce {α β : Sort u} : (α ⇝ β) → (α → β) := trans
 
-def idtoeqv {α β : Type u} (p : α ⇝ β) : α ≃ β :=
-transportconst (<i> α ≃ p # i) (equiv.id α)
+def idtoeqv {α β : Sort u} (p : α ⇝ β) : α ≃ β :=
+trans (<i> α ≃ p # i) (equiv.id α)
 
-def test_eta {α : Type u} {a b : α} (p : a ⇝ b) : p ⇝ p := rfl
-def face₀ {α : Type u} {a b : α} (p : a ⇝ b) : α := p # i₀
-def face₁ {α : Type u} {a b : α} (p : a ⇝ b) : α := p # i₁
+def test_eta {α : Sort u} {a b : α} (p : a ⇝ b) : p ⇝ p := rfl
+def face₀ {α : Sort u} {a b : α} (p : a ⇝ b) : α := p # i₀
+def face₁ {α : Sort u} {a b : α} (p : a ⇝ b) : α := p # i₁
 
-def comp_test₀ {α : Type u} {a b : α} (p : a ⇝ b) : (p # i₀) ⇝ a := rfl
-def comp_test₁ {α : Type u} {a b : α} (p : a ⇝ b) : (p # i₁) ⇝ b := rfl
+def comp_test₀ {α : Sort u} {a b : α} (p : a ⇝ b) : (p # i₀) ⇝ a := rfl
+def comp_test₁ {α : Sort u} {a b : α} (p : a ⇝ b) : (p # i₁) ⇝ b := rfl
 
 -- fail
 --def symm_test {α : Type u} {a b : α} (p : a ⇝ b) : (p⁻¹)⁻¹ ⇝ p := rfl
-@[trans] def trans {α : Type u} {a b c : α}
+@[trans] def composition {α : Sort u} {a b c : α}
   (p : a ⇝ b) (q : b ⇝ c) : a ⇝ c := subst q p
 
-infix ⬝ := trans
+infix ⬝ := composition
 
 -- this will be replaced by a more general version in future
-def comp {α : Type u} {a b c d : α}
+def comp {α : Sort u} {a b c d : α}
   (bottom : b ⇝ c) (left : b ⇝ a) (right : c ⇝ d) : a ⇝ d :=
 left⁻¹ ⬝ bottom ⬝ right
 
@@ -59,8 +66,33 @@ def conn_and {α : Sort u} {a b : α} (p : a ⇝ b) :
   LineP (λ i, a ⇝ p # i) :=
 λ i, <j> p # i ∧ j
 
-def PathP (σ : I → Type u) (a : σ i₀) (b : σ i₁) :=
+def PathP (σ : I → Sort u) (a : σ i₀) (b : σ i₁) :=
 Path (subst seg_path a) b
+
+def singl {α : Sort u} (a : α) :=
+Σ' (x : α), a ⇝ x
+
+def eta {α : Sort u} (a : α) : singl a := ⟨a, refl a⟩
+
+-- dirty way to define J elimination rule
+def hrec {β : I → Sort u}
+  (b₀ : β i₀) (b₁ : β i₁) (s : b₀ == b₁)
+  (x : I) : β x :=
+@quot.hrec_on bool (λ _ _, true) β x
+  (λ i, bool.rec_on i b₀ b₁)
+  (λ a b _,
+    begin simp, induction a; induction b; simp,
+          apply s, symmetry, apply s end)
+
+def refl_contr {α : Sort u} {a b : α} (p : a ⇝ b) : LineP (λ i, a ⇝ p # i) :=
+hrec (refl a) p (begin
+  cases p with f, unfold refl,
+  apply heq.map, funext,
+  refine interval.hrec _ _ i,
+  { reflexivity },
+  { apply ground_zero.support.truncation,
+    apply eq.map, exact seg }
+end)
 
 /-
 This doesn’t pass typechecking.
@@ -74,27 +106,8 @@ def J {α : Type u} {a : α} {π : Π (b : α), a ⇝ b → Type u}
 transport (<i> π (p # i) (<j> p # i ∧ j)) h
 -/
 
--- dirty way to define J elimination rule
-def hrec {β : I → Sort u}
-  (b₀ : β i₀) (b₁ : β i₁) (s : b₀ == b₁)
-  (x : I) : β x :=
-@quot.hrec_on bool (λ _ _, true) β x
-  (λ i, bool.rec_on i b₀ b₁)
-  (λ a b _,
-    begin simp, induction a; induction b; simp,
-          apply s, symmetry, apply s end)
-
-def J {α : Type u} {a : α} {π : Π (b : α), a ⇝ b → Type u}
+def J {α : Sort u} {a : α} {π : Π (b : α), a ⇝ b → Type u}
   (h : π a (refl a)) {b : α} (p : a ⇝ b) : π b p :=
-let dsingl : LineP (λ i, a ⇝ p # i) :=
-hrec (refl a) p (begin
-  cases p with f, unfold refl,
-  apply heq.map, funext,
-  refine interval.hrec _ _ i,
-  { reflexivity },
-  { apply ground_zero.support.truncation,
-    apply eq.map, exact seg }
-end) in
-transportconst (<i> π (p # i) (dsingl i)) h
+trans (<i> π (p # i) (refl_contr p i)) h
 
 end ground_zero.cubical.path
