13a944bde906e008fb354eab62e9468d252baf6a
Siegmentation <siegmentationfault@yandex.ru>
2018-12-28 23:04:37 +0700

    fast fix

--- ground_zero/cubical/V.lean
+++ ground_zero/cubical/V.lean
@@ -4,10 +4,10 @@ open ground_zero.HITs
 namespace ground_zero.cubical
 universes u v
 
-def V (i : I) {α β : Sort u} (e : α ≃ β) : I → Sort u :=
-interval.rec α β (ground_zero.ua e)
+def V (i : I) {α β : Sort u} (e : α ≃ β) : Sort u :=
+interval.rec α β (ground_zero.ua e) i
 
 -- why it isn’t need to be marked as noncomputable??
-def ua {α β : Sort u} (e : α ≃ β) := <i> V i e
+def ua {α β : Sort u} (e : α ≃ β) : α ⇝ β := <i> V i e
 
 end ground_zero.cubical
