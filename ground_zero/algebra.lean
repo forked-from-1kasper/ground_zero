d077b92a511f77d29bb8bd78f66e9c964431bfd7
Siegmentation <siegmentationfault@yandex.ru>
2019-04-02 19:36:32 +0700

    calculation proofs

--- ground_zero/algebra.lean
+++ ground_zero/algebra.lean
@@ -10,47 +10,77 @@ universes u v
 class magma (α : Type u) :=
 (mul : α → α → α)
 
+infix ` · `:70 := magma.mul
+
 class pointed_magma (α : Type u) extends magma α :=
 (e : α)
 
 class monoid (α : Type u) extends magma α :=
 (truncation : hset α) (e : α)
-(left_unit : Π (x : α), mul x e = x)
-(right_unit : Π (x : α), mul e x = x)
-(assoc : Π (x y z : α), mul x (mul y z) = mul (mul x y) z)
-
-infix ` · `:70 := magma.mul
+(right_unit : Π (x : α), x · e = x)
+(left_unit : Π (x : α), e · x = x)
+(assoc : Π (x y z : α), x · (y · z) = (x · y) · z)
 
 class group (α : Type u) extends monoid α :=
 (inv : α → α)
-(left_inv : Π (x : α), x · (inv x) = e)
-(right_inv : Π (x : α), (inv x) · x = e)
+(right_inv : Π (x : α), x · inv x = e)
+(left_inv : Π (x : α), inv x · x = e)
 
 def e {α : Type u} [group α] : α := monoid.e α
+instance {α : Type u} [monoid α] : has_one α := ⟨monoid.e α⟩
+instance {α : Type u} [group α] : has_inv α := ⟨group.inv⟩
 
 theorem group_unit_is_unique {α : Type u} [group α] (e' : α)
-  (left_unit' : Π x, x · e' = x)
-  (right_unit' : Π x, e' · x = x)
+  (right_unit' : Π x, x · e' = x)
+  (left_unit' : Π x, e' · x = x)
   (H : e = e' → empty) : empty := begin
-  have p := monoid.left_unit e',
-  have q := right_unit' e,
+  have p := monoid.right_unit e',
+  have q := left_unit' e,
   exact H (q⁻¹ ⬝ p)
 end
 
 theorem square_is_unique {α : Type u} [group α] (x : α)
-  (h : x · x = x) : x = e := begin
-  transitivity, { symmetry, apply monoid.right_unit },
-  transitivity, {
-    apply ground_zero.types.eq.map (· x),
-    symmetry, apply group.right_inv x
-  },
-  transitivity, { symmetry, apply monoid.assoc },
-  transitivity, { apply ground_zero.types.eq.map, exact h },
-  apply group.right_inv
-end
+  (h : x · x = x) : x = 1 := calc
+    x = e · x : begin symmetry, apply monoid.left_unit end
+  ... = (x⁻¹ · x) · x :
+        begin
+          apply ground_zero.types.eq.map (· x),
+          symmetry, apply group.left_inv x
+        end
+  ... = x⁻¹ · (x · x) : begin symmetry, apply monoid.assoc end
+  ... = x⁻¹ · x : magma.mul x⁻¹ # h
+  ... = 1 : by apply group.left_inv
 
-theorem identity_inv {α : Type u} [group α] : e = group.inv e :> α :=
-(group.left_inv e)⁻¹ ⬝ monoid.right_unit (group.inv e)
+theorem inv_of_inv {α : Type u} [group α] (x : α) : (x⁻¹)⁻¹ = x := calc
+  (x⁻¹)⁻¹ = e · (x⁻¹)⁻¹ : begin symmetry, apply monoid.left_unit end
+      ... = (x · x⁻¹) · (x⁻¹)⁻¹ :
+            begin
+              apply ground_zero.types.eq.map (· x⁻¹⁻¹),
+              symmetry,
+              apply group.right_inv x
+            end
+      ... = x · (x⁻¹ · x⁻¹⁻¹) : begin symmetry, apply monoid.assoc end
+      ... = x · e : magma.mul x # (group.right_inv x⁻¹)
+      ... = x : monoid.right_unit x
+
+theorem reduce_left {α : Type u} [group α] (a b c : α)
+  (h : c · a = c · b) : a = b := calc
+    a = e · a         : (monoid.left_unit a)⁻¹
+  ... = (c⁻¹ · c) · a : (· a) # (group.left_inv c)⁻¹
+  ... = c⁻¹ · (c · a) : begin symmetry, apply monoid.assoc end
+  ... = c⁻¹ · (c · b) : magma.mul c⁻¹ # h
+  ... = (c⁻¹ · c) · b : by apply monoid.assoc
+  ... = e · b         : (· b) # (group.left_inv c)
+  ... = b             : monoid.left_unit b
+
+theorem identity_inv {α : Type u} [group α] : e = e⁻¹ :> α :=
+(group.right_inv e)⁻¹ ⬝ monoid.left_unit e⁻¹
+
+def Zentrum (α : Type u) [group α] :=
+Σ (z : α), Π g, z · g = g · z
+
+def commutator {α : Type u} [group α] (g h : α) :=
+g⁻¹ · h⁻¹ · g · h
 
 section
   variables {α : Type u} {β : Type v} [group α] [group β]
