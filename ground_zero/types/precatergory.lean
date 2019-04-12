ab26cb0a9c0f9ee52b99f24d55acf37a603e282a
Siegmentation <siegmentationfault@yandex.ru>
2019-04-12 22:26:42 +0700

    categories

--- ground_zero/types/precatergory.lean
+++ ground_zero/types/precatergory.lean
@@ -1,13 +1,14 @@
-import ground_zero.HITs.graph
-open ground_zero.HITs ground_zero.types
+import ground_zero.HITs.graph ground_zero.HITs.interval
+open ground_zero.HITs ground_zero.types ground_zero.theorems.functions
+open ground_zero.HITs.interval ground_zero.types.equiv
 
-namespace ground_zero.types
 hott theory
 
-universe u
+namespace ground_zero.types
+universes u v
 
 structure precategory (α : Sort u) :=
-(hom : α → α → Sort u)
+(hom : α → α → Sort v)
 (id {a : α} : hom a a)
 (comp {a b c : α} : hom b c → hom a b → hom a c)
 (infix ∘ := comp)
@@ -16,14 +17,24 @@ structure precategory (α : Sort u) :=
 (assoc {a b c d : α} : Π (f : hom a b) (g : hom b c) (h : hom c d),
   h ∘ (g ∘ f) = (h ∘ g) ∘ f)
 
-def cat_graph {α : Sort u} (C : precategory α) := graph C.hom
+namespace precategory
+  def cat_graph {α : Sort u} (𝒞 : precategory α) := graph (hom 𝒞)
 
-theorem type_is_precategory (α : Sort u) : precategory α :=
-{ hom := (=),
-  id := ground_zero.types.eq.refl,
-  comp := λ a b c p q, q ⬝ p,
-  id_left := λ a b p, (eq.refl_right p)⁻¹,
-  id_right := λ a b p, (eq.refl_left p)⁻¹,
-  assoc := λ a b c d f g h, (eq.assoc f g h)⁻¹ }
+  def Path (α : Sort u) : precategory α :=
+  { hom := (=),
+    id := ground_zero.types.eq.refl,
+    comp := λ a b c p q, q ⬝ p,
+    id_left := λ a b p, (eq.refl_right p)⁻¹,
+    id_right := λ a b p, (eq.refl_left p)⁻¹,
+    assoc := λ a b c d f g h, (eq.assoc f g h)⁻¹ }
+
+  def Top : precategory (Sort u) :=
+  { hom := (→),
+    id := @idfun,
+    comp := @function.comp,
+    id_left := λ a b f, funext (homotopy.id f),
+    id_right := λ a b f, funext (homotopy.id f),
+    assoc := λ a b c d f g h, eq.rfl }
+end precategory
 
 end ground_zero.types
