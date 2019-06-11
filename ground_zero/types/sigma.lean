cfaae15a046251397e4a99ae236ffe5f6daaa0b2
Siegmentation <siegmentationfault@yandex.ru>
2019-06-11 16:13:15 +0700

    leg

--- ground_zero/types/sigma.lean
+++ ground_zero/types/sigma.lean
@@ -1,4 +1,7 @@
+import ground_zero.types.equiv
+
 namespace ground_zero.types
+hott theory
 
 universes u v w
 
@@ -14,6 +17,16 @@ namespace sigma
   def ind {π : psigma β → Type w} (g : Π (a : α) (b : β a), π ⟨a, b⟩) :
     Π (x : psigma β), π x
   | ⟨a, b⟩ := g a b
+
+  lemma prod {α : Sort u} {β : α → Sort v} {u v : psigma β}
+    (h : u.fst = v.fst) (g : equiv.subst h u.snd = v.snd) : u = v := begin
+    cases u with x u, cases v with y v,
+    fapply equiv.transport (λ (v : β y), ⟨x, u⟩ = ⟨y, v⟩ :> psigma β),
+    exact g,
+    apply @eq.rec α x (λ (y : α) (h : x = y),
+      ⟨x, u⟩ = ⟨y, equiv.subst h u⟩ :> psigma β),
+    trivial
+  end
 end sigma
 
 end ground_zero.types
