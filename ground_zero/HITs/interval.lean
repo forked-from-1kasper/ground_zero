2a30c2d00434fc274bd4b194be1ab7cc5700e66d
Siegmentation <siegmentationfault@yandex.ru>
2019-06-11 22:08:53 +0700

    arrow

--- ground_zero/HITs/interval.lean
+++ ground_zero/HITs/interval.lean
@@ -145,6 +145,8 @@ namespace interval
   def fibration (α : Sort u) (β : Sort v) :=
   Σ' (p : α → β), lifting_property p
 
+  notation α ` ↠ ` β := fibration α β
+
   lemma lifting {α : Sort u} {β : α → Sort v} (f : I → α)
     (u : β (f 0)) : @leg (psigma β) ⟨f 0, u⟩ :=
   @leg.lam (psigma β) (λ i, ⟨f i,
@@ -152,7 +154,7 @@ namespace interval
       (types.equiv.path_over_subst types.eq.rfl) i⟩)
 
   theorem type_family {α : Sort u} (β : α → Sort v) :
-    fibration (Σ' x, β x) α := begin
+    (Σ' x, β x) ↠ α := begin
     existsi psigma.fst, intros x f,
     cases x with x u, cases f with f u, apply lifting
   end
