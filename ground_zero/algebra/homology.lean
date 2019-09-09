1b1d2fb0a691825f3d349492f2e68451353da62f
Siegmentation <siegmentationfault@yandex.ru>
2019-09-09 16:49:23 +0700

    define B and Z properly

--- ground_zero/algebra/homology.lean
+++ ground_zero/algebra/homology.lean
@@ -8,10 +8,19 @@ namespace ground_zero.algebra.homology
 universes u
 
 structure chain_complex :=
-(K : ℕ → Type u) [is_group : Π n, grp (K n)] [commutes : Π n, abelian (K n)]
+(K : ℕ → Type u)
+(is_group : Π n, grp (K n))
+(commutes : Π n, abelian (K n))
 (d : Π n, K (nat.succ n) ⤳ K n)
 (condition : Π n, d n ⋅ d (nat.succ n) = 0)
-(B := λ n, Ker (d n))
-(Z := λ n, im (d $ nat.succ n))
+
+def B (C : chain_complex) :=
+λ n, @ker _ _ (C.is_group (nat.succ n)) (C.is_group n) (C.d n)
+
+def Z (C : chain_complex) :=
+λ n, @Im _ _
+  (C.is_group (nat.succ (nat.succ n)))
+  (C.is_group (nat.succ n))
+  (C.d (nat.succ n))
 
 end ground_zero.algebra.homology
