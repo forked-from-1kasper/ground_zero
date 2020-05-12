3774f78df27e3470aef7bd8c1f29aca3946ff231
Siegmentation <siegmentationfault@yandex.ru>
2020-05-12 22:08:26 +0700

    fix

--- ground_zero/HITs/simplicial.lean
+++ ground_zero/HITs/simplicial.lean
@@ -12,13 +12,20 @@ hott theory
 namespace ground_zero.HITs
 universes u v w
 
+def neq {α : Type u} (a b : α) : Type u := a = b → (𝟎 : Type)
+
 def fin : ℕ → Type
 | 0 := empty
 | (n + 1) := coproduct ground_zero.types.unit.{0} (fin n)
 
 def filled  (n : ℕ) := ∥fin n∥
 
-def simplex (n : ℕ) := generalized (fin n)
-def simplex.elem {n : ℕ} : fin n → simplex n := generalized.incl
+inductive coupling.rel (α : Type u) : α → α → Type u
+| mk {} : Π (a b : α), neq a b → coupling.rel a b
+
+def coupling (α : Type u) := graph (generalized.rel α)
+
+def simplex (n : ℕ) := coupling (fin n)
+def simplex.elem {n : ℕ} : fin n → simplex n := graph.elem
 
 end ground_zero.HITs
