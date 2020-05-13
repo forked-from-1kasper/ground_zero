03802b9e0ec7fe60c78cec42bf0ed18abebe5472
Siegmentation <siegmentationfault@yandex.ru>
2020-05-13 22:56:30 +0700

    tags

--- ground_zero/HITs/simplicial.lean
+++ ground_zero/HITs/simplicial.lean
@@ -16,14 +16,14 @@ def filled (n : ℕ) := ∥fin n∥
 
 def network (α : Type u) := graph (@neq α)
 
-def network.decode {α : Type u} (H : prop α) : network α → α := begin
+@[hott] def network.decode {α : Type u} (H : prop α) : network α → α := begin
   fapply graph.ind,
   { exact id },
   { intros x y G, apply ground_zero.proto.empty.elim,
     apply G, apply H }
 end
 
-def network.prop {α : Type u} (H : prop α) : prop (network α) := begin
+@[hott] def network.prop {α : Type u} (H : prop α) : prop (network α) := begin
   intros x y, fapply graph.ind _ _ x; clear x; intro x,
   { fapply graph.ind _ _ y; clear y; intro y,
     { apply eq.map, apply H },
@@ -33,7 +33,7 @@ def network.prop {α : Type u} (H : prop α) : prop (network α) := begin
     apply G, apply H }
 end
 
-def network.proplem {α : Type u} (H : prop α) : α ≃ network α := begin
+@[hott] def network.proplem {α : Type u} (H : prop α) : α ≃ network α := begin
   apply ground_zero.structures.prop_equiv_lemma,
   apply H, apply network.prop H,
   apply graph.elem, apply network.decode H
@@ -45,7 +45,7 @@ def hull.elem {n : ℕ} : fin n → hull n := graph.elem
 inductive tetrahedron (n : ℕ) (α : Type u) : vect α n → Type u
 | refl {} (a : α) : tetrahedron (vect.constant a n)
 
-def tetrahedron.map {n : ℕ} {α : Type u} {β : Type v} (f : α → β)
+@[hott] def tetrahedron.map {n : ℕ} {α : Type u} {β : Type v} (f : α → β)
   (x : vect α n) (p : tetrahedron n α x) : tetrahedron n β (vect.map f x) := begin
   induction p, apply ground_zero.types.equiv.transport,
   { symmetry, apply ground_zero.vect.const_map },
