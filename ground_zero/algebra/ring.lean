819ba124eab803b4166fe549953686d1d2466f64
Siegmentation Fault <siegmentationfault@yandex.ru>
2020-09-26 23:11:55 +0700

    ???

--- ground_zero/algebra/ring.lean
+++ ground_zero/algebra/ring.lean
@@ -11,18 +11,29 @@ structure ring extends group :=
 (distrib_left  : Π a b c, ψ a (φ b c) = φ (ψ a b) (ψ a c))
 (distrib_right : Π a b c, ψ (φ a b) c = φ (ψ a c) (ψ b c))
 
-def ring.carrier (T : ring) := T.α.fst
-def ring.proper (T : ring) := T.to_group.proper
+section
+  variable (T : ring)
+  def ring.carrier := T.α.fst
+  def ring.zero    := T.to_group.e
+  def ring.proper  := T.to_group.proper
+end
 
-@[class] def ring.assoc (T : ring) :=
-Π a b c, T.ψ (T.ψ a b) c = T.ψ a (T.ψ b c)
+class ring.assoc (T : ring) :=
+(mul_assoc : Π a b c, T.ψ (T.ψ a b) c = T.ψ a (T.ψ b c))
 
-@[class] def ring.comm (T : ring) :=
-Π a b, T.ψ a b = T.ψ b a
+class ring.comm (T : ring) :=
+(mul_comm : Π a b, T.ψ a b = T.ψ b a)
 
 class ring.identity (T : ring) :=
-(id       : T.carrier)
-(mul_unit : Π x, T.proper x → T.ψ x id = x)
-(unit_mul : Π x, T.proper x → T.ψ id x = x)
+(idm      : T.carrier)
+(mul_unit : Π x, T.proper x → T.ψ x idm = x)
+(unit_mul : Π x, T.proper x → T.ψ idm x = x)
+
+class ring.invertible (T : ring) extends ring.identity T :=
+(inv           : T.carrier → T.carrier)
+(inv_zero      : inv T.zero = T.zero)
+(mul_inv_right : Π x, T.proper x → T.ψ x (inv x) = idm)
+
+class field (T : ring) extends ring.assoc T, ring.comm T, ring.invertible T
 
 end ground_zero.algebra
