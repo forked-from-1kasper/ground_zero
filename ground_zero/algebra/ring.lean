aea7623bb77f42fc3fe629258035cb89cf2e2192
Siegmentation Fault <siegmentationfault@yandex.ru>
2020-09-26 22:45:35 +0700

    more properties

--- ground_zero/algebra/ring.lean
+++ ground_zero/algebra/ring.lean
@@ -12,6 +12,7 @@ structure ring extends group :=
 (distrib_right : Π a b c, ψ (φ a b) c = φ (ψ a c) (ψ b c))
 
 def ring.carrier (T : ring) := T.α.fst
+def ring.proper (T : ring) := T.to_group.proper
 
 @[class] def ring.assoc (T : ring) :=
 Π a b c, T.ψ (T.ψ a b) c = T.ψ a (T.ψ b c)
@@ -21,8 +22,8 @@ def ring.carrier (T : ring) := T.α.fst
 
 class ring.identity (T : ring) :=
 (id       : T.carrier)
-(mul_unit : Π x, T.ψ x id = x)
-(unit_mul : Π x, T.ψ id x = x)
+(mul_unit : Π x, T.proper x → T.ψ x id = x)
+(unit_mul : Π x, T.proper x → T.ψ id x = x)
 
 structure field :=
 (G H : group)
@@ -41,14 +42,37 @@ def field.add (F : field) : F.carrier → F.carrier → F.carrier := F.G.φ
 def field.mul (F : field) (x y : F.carrier) : F.carrier :=
 F.η (F.H.φ (F.μ x) (F.μ y))
 
-def field.distrib_right (F : field) (a b c : F.carrier) :
+def field.one (F : field) : F.carrier := F.η F.H.e
+
+@[hott] def field.distrib_right (F : field) (a b c : F.carrier) :
   F.mul (F.add a b) c = F.add (F.mul a c) (F.mul b c) := begin
   transitivity, { apply map F.η, apply F.mul_comm },
   transitivity, { apply F.distrib_left },
   apply ground_zero.types.equiv.bimap; apply map F.η; apply F.mul_comm
 end
 
-def field.to_ring (F : field) : ring :=
+@[hott] def field.to_ring (F : field) : ring :=
 ⟨F.G, F.mul, F.add_comm, F.distrib_left, F.distrib_right⟩
+
+instance (F : field) : ring.assoc F.to_ring := begin
+  intros a b c, apply map F.η,
+  transitivity, apply map (λ x, F.H.φ x (F.μ c)), apply F.incl,
+  transitivity, apply F.H.mul_assoc,
+  symmetry, apply map, apply F.incl
+end
+
+instance (F : field) : ring.comm F.to_ring :=
+begin intros a b, apply map F.η, apply F.mul_comm end
+
+instance (F : field) : ring.identity F.to_ring := begin
+  fapply ring.identity.mk, exact F.one,
+  { intros x p, symmetry, transitivity, { apply (F.surj x p)⁻¹ },
+    symmetry, apply map F.η,
+    transitivity, { apply map, apply F.incl }, apply F.H.mul_one },
+  { intros x p, symmetry, transitivity, { apply (F.surj x p)⁻¹ },
+    symmetry, apply map F.η,
+    transitivity, { apply map (λ y, F.H.φ y (F.μ x)), apply F.incl },
+    apply F.H.one_mul }
+end
 
 end ground_zero.algebra
