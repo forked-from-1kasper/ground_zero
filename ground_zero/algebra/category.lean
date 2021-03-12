5bca1080ee0130c9c69fc7c890fd1b7d5d2f344d
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-03-12 23:22:01 +0700

    algebraic definition of category

---
+++ ground_zero/algebra/category.lean
@@ -0,0 +1,50 @@
+import ground_zero.algebra.basic
+
+hott theory
+
+namespace ground_zero.algebra
+
+universe u
+
+namespace precategory
+  inductive arity : Type
+  | left | right | mul | bottom
+  open arity
+
+  def signature : arity + ⊥ → ℕ
+  | (sum.inl mul)    := 2
+  | (sum.inl left)   := 1
+  | (sum.inl right)  := 1
+  | (sum.inl bottom) := 0
+end precategory
+
+def precategory : Type (u + 1) :=
+Alg.{0 0 u 0} precategory.signature
+
+namespace precategory
+  def bottom (𝒞 : precategory) : 𝒞.carrier :=
+  𝒞.op arity.bottom ★
+
+  abbreviation undefined {𝒞 : precategory} :=
+  𝒞.bottom
+
+  notation `∄` := undefined
+
+  def μ (𝒞 : precategory) : 𝒞.carrier → 𝒞.carrier → 𝒞.carrier :=
+  λ x y, 𝒞.op arity.mul (x, y, ★)
+
+  def lid (𝒞 : precategory) : 𝒞.carrier → 𝒞.carrier :=
+  λ x, 𝒞.op arity.left (x, ★)
+
+  def rid (𝒞 : precategory) : 𝒞.carrier → 𝒞.carrier :=
+  λ x, 𝒞.op arity.right (x, ★)
+end precategory
+
+class category (𝒞 : precategory) :=
+(bottom_left  : Π a, 𝒞.μ ∄ a = ∄)
+(bottom_right : Π a, 𝒞.μ a ∄ = ∄)
+(lid_comp     : Π a, 𝒞.μ (𝒞.lid a) a = a)
+(rid_comp     : Π a, 𝒞.μ a (𝒞.rid a) = a)
+(mul_assoc    : Π a b c, 𝒞.μ (𝒞.μ a b) c = 𝒞.μ a (𝒞.μ b c))
+
+end ground_zero.algebra
