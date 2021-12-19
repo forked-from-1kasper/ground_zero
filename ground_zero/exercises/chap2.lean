00a993db1a2c15e28c27c86a4564287e6157dc6c
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-12-19 22:14:57 +0700

    2.3

--- ground_zero/exercises/chap2.lean
+++ ground_zero/exercises/chap2.lean
@@ -15,17 +15,17 @@ section
   variables {α : Type u} {a b c : α}
 
   @[hott] def trans₁ (p : a = b) (q : b = c) : a = c :=
-  begin induction p, induction q, reflexivity end
+  @Id.rec α a (λ x _, x = c → a = c) (@Id.rec α a (λ x _, a = x) (idp a) c) b p q
 
   infixl ` ⬝₁ `:99 := trans₁
 
   @[hott] def trans₂ (p : a = b) (q : b = c) : a = c :=
-  begin induction p, exact q end
+  @Id.rec α a (λ x _, x = c → a = c) idfun b p q
 
   infixl ` ⬝₂ `:99 := trans₂
 
   @[hott] def trans₃ (p : a = b) (q : b = c) : a = c :=
-  begin induction q, exact p end
+  @Id.rec α b (λ x _, a = b → a = x) idfun c q p
 
   infixl ` ⬝₃ `:99 := trans₃
 
@@ -42,8 +42,28 @@ end
 -- exercise 2.2
 
 section
-  variables {A : Type u} {a b c : A} (p : a = b) (q : b = c)
+  variables {α : Type u} {a b c : α} (p : a = b) (q : b = c)
 
   @[hott] example : eq₁₂ p q ⬝ eq₂₃ p q = eq₁₃ p q :=
+  begin induction p, induction q, reflexivity end
+end
+
+-- exercise 2.3
+
+section
+  variables {α : Type u} {a b c : α}
+
+  @[hott] def trans₄ (p : a = b) (q : b = c) : a = c :=
+  @Id.rec α b (λ x _, a = b → a = x) (@Id.rec α a (λ x _, a = x) (idp a) b) c q p
+
+  infixl ` ⬝₄ `:99 := trans₄
+
+  /-
+  example (p : a = b) (q : b = c) : p ⬝₁ q = p ⬝₄ q := idp _
+  example (p : a = b) (q : b = c) : p ⬝₂ q = p ⬝₄ q := idp _
+  example (p : a = b) (q : b = c) : p ⬝₃ q = p ⬝₄ q := idp _
+  -/
+
+  @[hott] example (p : a = b) (q : b = c) : p ⬝₁ q = p ⬝₄ q :=
   begin induction p, induction q, reflexivity end
 end
