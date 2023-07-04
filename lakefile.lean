1a171e39f84d522f738e93e9c4cee20ba0e4431d
Siegmentation Fault <siegmentationfault@yandex.ru>
2023-07-04 18:46:39 +0700

    lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,10 +1,9 @@
 import Lake
 open Lake DSL
 
-package GroundZero {
-  moreLeanArgs := #["-Dlinter.unusedVariables=false",
-                    "-DautoImplicit=false"]
-}
+def leanArgs := #["-Dlinter.unusedVariables=false", "-DautoImplicit=false"]
+
+package GroundZero { moreServerArgs := leanArgs, moreLeanArgs := leanArgs }
 
 @[default_target]
 lean_lib GroundZero
