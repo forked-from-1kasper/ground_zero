d70c693210f0439f0dee4ad89e05a209da91aca9
Siegmentation Fault <siegmentationfault@yandex.ru>
2023-07-22 13:54:11 +0700

    3.10

--- GroundZero/Exercises/Chap3.lean
+++ GroundZero/Exercises/Chap3.lean
@@ -8,47 +8,104 @@ open GroundZero.Theorems
 open GroundZero.Proto
 
 open GroundZero.Structures (dec prop contr propset)
+open GroundZero.Types.Id (ap)
 
 universe u v
 
 -- exercise 3.9
 
-hott def lemTrue {A : Type u} (H : prop A) (lem : LEM₋₁) (x : A) : lem A H = Sum.inl x :=
-begin
-  match lem A H with | Sum.inl y => _ | Sum.inr φ => _;
-  apply Id.map; apply H; apply Empty.elim; apply φ x
-end
+namespace «3.9»
+  hott def lemTrue {A : Type u} {H : prop A} {lem : LEM₋₁} (x : A) : lem A H = Sum.inl x :=
+  begin
+    match lem A H with | Sum.inl y => _ | Sum.inr φ => _;
+    { apply Id.map; apply H }; { apply Empty.elim; apply φ x }
+  end
 
-hott def lemFalse {A : Type u} (H : prop A) (lem : LEM₋₁) (φ : ¬A) : lem A H = Sum.inr φ :=
-begin
-  match lem A H with | Sum.inl x => _ | Sum.inr ψ => _;
-  apply Empty.elim; apply φ x; apply Id.map; apply Structures.notIsProp
-end
+  hott def lemFalse {A : Type u} {H : prop A} {lem : LEM₋₁} (φ : ¬A) : lem A H = Sum.inr φ :=
+  begin
+    match lem A H with | Sum.inl x => _ | Sum.inr ψ => _;
+    { apply Empty.elim; apply φ x };
+    { apply Id.ap; apply Structures.notIsProp }
+  end
 
-hott lemma propsetInhIsProp (A : propset) : prop A.1 := A.2
+  hott def Ωelim (lem : LEMprop.{u}) : propset.{u} → 𝟐 :=
+  λ w, Coproduct.elim (λ _, true) (λ _, false) (lem w.1 w.2)
 
-hott theorem lemImplPropEqvBool : LEMprop.{u} → propset.{u} ≃ 𝟐 :=
-begin
-  intro lem; fapply Sigma.mk;
-  { intro w; exact Coproduct.elim (λ _, true) (λ _, false) (lem w.1 w.2) };
+  hott def Ωintro : 𝟐 → propset :=
+  Bool.elim ⟨𝟎, Structures.emptyIsProp⟩ ⟨𝟏, Structures.unitIsProp⟩
 
-  apply Qinv.toBiinv; fapply Sigma.mk;
-  { fapply Bool.elim; exact ⟨𝟎, Structures.emptyIsProp⟩; exact ⟨𝟏, Structures.unitIsProp⟩ }; apply Prod.mk;
+  hott lemma propsetInhIsProp (A : propset) : prop A.1 := A.2
 
-  { intro b; induction b using Bool.casesOn;
-    { transitivity; apply Id.map (Coproduct.elim _ _); apply lemFalse; apply Empty.elim; reflexivity };
-    { transitivity; apply Id.map (Coproduct.elim _ _); apply lemTrue; exact ★; reflexivity } };
+  hott lemma Ωlinv (lem : LEMprop.{u}) : Ωelim lem ∘ Ωintro ~ idfun
+  | false => ap (Coproduct.elim _ _) (lemFalse Empty.elim)
+  | true  => ap (Coproduct.elim _ _) (lemTrue ★)
 
-  { intro w; apply Sigma.prod; apply Structures.propIsProp;
-    match lem w.1 w.2 with | Sum.inl x => _ | Sum.inr φ => _;
+  hott lemma Ωrinv (lem : LEMprop.{u}) : Ωintro ∘ Ωelim lem ~ idfun :=
+  begin
+    intro w; apply Equiv.propset.Id; match lem w.1 w.2 with | Sum.inl x => _ | Sum.inr φ => _;
 
     transitivity; apply Id.map; apply Id.map (Bool.elim _ _); apply Id.map (Coproduct.elim _ _);
     apply lemTrue; exact x; symmetry; apply ua; apply Structures.contrEquivUnit;
     fapply Sigma.mk; exact x; intro y; apply w.2;
 
     transitivity; apply Id.map; apply Id.map (Bool.elim _ _); apply Id.map (Coproduct.elim _ _);
-    apply lemFalse; exact φ; symmetry; apply ua; apply uninhabitedType; exact Empty.elim ∘ φ }
-end
+    apply lemFalse; exact φ; symmetry; apply ua; apply uninhabitedType; exact Empty.elim ∘ φ
+  end
+
+  hott theorem lemImplPropEqvBool (lem : LEMprop.{u}) : propset.{u} ≃ 𝟐 :=
+  ⟨Ωelim lem, Qinv.toBiinv _ ⟨Ωintro, (Ωlinv lem, Ωrinv lem)⟩⟩
+end «3.9»
+
+-- exercise 3.10
+
+namespace «3.10»
+  open «3.9»
+
+  inductive Resize (A : Type u) : Type (max u v)
+  | intro : A → Resize A
+
+  hott def Resize.elim {A : Type u} : Resize A → A
+  | intro w => w
+
+  hott theorem Resize.equiv (A : Type u) : A ≃ Resize.{u, v} A :=
+  ⟨Resize.intro, Qinv.toBiinv _ ⟨Resize.elim, (λ (Resize.intro _), idp _, idp)⟩⟩
+
+  hott lemma Resize.prop {A : Type u} (H : prop A) : prop (Resize.{u, v} A) :=
+  Structures.propRespectsEquiv.{u, max u v} (Resize.equiv A) H
+
+  hott def ResizeΩ : propset.{u} → propset.{max u v} :=
+  λ w, ⟨Resize.{u, v} w.1, Resize.prop w.2⟩
+
+  hott lemma lemCumulativity (lem : LEMprop.{max u v}) : LEMprop.{u} :=
+  λ A H, match lem (Resize.{u, v} A) (Resize.prop H) with
+  | Sum.inl x => Sum.inl (Resize.elim x)
+  | Sum.inr φ => Sum.inr (φ ∘ Resize.intro)
+
+  hott corollary lemSucCumulativity : LEMprop.{u + 1} → LEMprop.{u} :=
+  lemCumulativity.{u, u + 1}
+
+  hott lemma lemImplPropUniverseEqv (lem : LEMprop.{max u v}) : propset.{u} ≃ propset.{max u v} :=
+  Equiv.trans (lemImplPropEqvBool (lemCumulativity.{u, v} lem))
+              (Equiv.symm (lemImplPropEqvBool lem))
+
+  hott lemma resizeUniqLem1 (lem : LEMprop.{max u v}) : (lemImplPropUniverseEqv.{u, v} lem).1 ∘ Ωintro ~ ResizeΩ.{u, v} ∘ Ωintro :=
+  begin
+    intro b; transitivity; apply ap Ωintro; apply Ωlinv; apply Equiv.propset.Id;
+    symmetry; apply ua; induction b using Bool.casesOn;
+    { apply uninhabitedType; exact Empty.elim ∘ Resize.elim };
+    { apply Structures.contrEquivUnit; existsi Resize.intro ★;
+      intro (Resize.intro b); apply ap; apply Structures.unitIsProp }
+  end
+
+  hott lemma resizeUniqLem2 (lem : LEMprop.{max u v}) : (lemImplPropUniverseEqv.{u, v} lem).1 ~ ResizeΩ.{u, v} :=
+  begin
+    intro w; transitivity; apply ap; symmetry; apply Ωrinv (lemCumulativity.{u, v} lem);
+    transitivity; apply resizeUniqLem1; apply ap ResizeΩ; apply Ωrinv
+  end
+
+  hott theorem lemImplResizing (lem : LEMprop.{max u v}) : biinv ResizeΩ :=
+  transport biinv (Theorems.funext (resizeUniqLem2.{u, v} lem)) (lemImplPropUniverseEqv lem).2
+end «3.10»
 
 -- exercise 3.19
 
