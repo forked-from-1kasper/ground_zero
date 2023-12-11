21e4a3e47334ca9f97c832f8bdb6b5048550eadb
Siegmentation Fault <siegmentationfault@yandex.ru>
2023-12-11 16:16:35 +0700

    clean up a bit

--- GroundZero/HITs/Pushout.lean
+++ GroundZero/HITs/Pushout.lean
@@ -17,26 +17,26 @@ universe u v w k
 section
   variable {A : Type u} {B : Type v} {C : Type k} (f : C → A) (g : C → B)
 
-  inductive Pushout.rel : (A ⊕ B) → (A ⊕ B) → Type k
+  inductive Pushout.rel : Sum A B → Sum A B → Type k
   | mk : Π (x : C), rel (Sum.inl (f x)) (Sum.inr (g x))
 
-  def Pushout := Graph (Pushout.rel f g)
+  hott definition Pushout := Graph (Pushout.rel f g)
 end
 
 namespace Pushout
   -- https://github.com/leanprover/lean2/blob/master/hott/hit/Pushout.hlean
   variable {A : Type u} {B : Type v} {C : Type k} {f : C → A} {g : C → B}
 
-  hott def inl (x : A) : Pushout f g :=
+  hott definition inl (x : A) : Pushout f g :=
   Graph.elem (Sum.inl x)
 
-  hott def inr (x : B) : Pushout f g :=
+  hott definition inr (x : B) : Pushout f g :=
   Graph.elem (Sum.inr x)
 
-  hott def glue (x : C) : @inl _ _ _ f g (f x) = inr (g x) :=
+  hott definition glue (x : C) : @inl _ _ _ f g (f x) = inr (g x) :=
   Graph.line (Pushout.rel.mk x)
 
-  hott def ind {P : Pushout f g → Type w} (inlπ : Π x, P (inl x)) (inrπ : Π x, P (inr x))
+  hott definition ind {P : Pushout f g → Type w} (inlπ : Π x, P (inl x)) (inrπ : Π x, P (inr x))
     (glueπ : Π x, inlπ (f x) =[glue x] inrπ (g x)) : Π x, P x :=
   begin
     fapply Graph.ind; { intro x; induction x using Sum.casesOn; apply inlπ; apply inrπ };
@@ -45,17 +45,17 @@ namespace Pushout
 
   attribute [eliminator] ind
 
-  hott def rec {D : Type w} (inlπ : A → D) (inrπ : B → D)
+  hott definition rec {D : Type w} (inlπ : A → D) (inrπ : B → D)
     (glueπ : Π x, inlπ (f x) = inrπ (g x)) : Pushout f g → D :=
   ind inlπ inrπ (λ x, pathoverOfEq (glue x) (glueπ x))
 
-  hott def indβrule {P : Pushout f g → Type w}
+  hott definition indβrule {P : Pushout f g → Type w}
     (inlπ : Π x, P (inl x)) (inrπ : Π x, P (inr x))
     (glueπ : Π x, inlπ (f x) =[glue x] inrπ (g x)) (x : C) :
     apd (ind inlπ inrπ glueπ) (glue x) = glueπ x :=
   @Graph.indβrule _ (rel f g) _ _ _ _ _ (rel.mk x)
 
-  hott def recβrule {D : Type w} (inlπ : A → D) (inrπ : B → D)
+  hott definition recβrule {D : Type w} (inlπ : A → D) (inrπ : B → D)
     (glueπ : Π x, inlπ (f x) = inrπ (g x)) (x : C) :
     ap (rec inlπ inrπ glueπ) (glue x) = glueπ x :=
   begin
